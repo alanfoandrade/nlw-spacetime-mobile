63affae9f5ca71263b76f8c72d26047f424f3b98
Alan Andrade <alan@fasters.com.br>
2023-05-21 15:15:48 -0300

    feat: add new memories page

--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,12 @@
-import { StatusBar } from 'expo-status-bar'
-import { styled } from 'nativewind'
 import React, { useEffect } from 'react'
 import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
-import { ImageBackground, View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import * as SecureStore from 'expo-secure-store'
 import { useRouter } from 'expo-router'
 
-import {
-  useFonts,
-  Roboto_400Regular,
-  Roboto_700Bold,
-} from '@expo-google-fonts/roboto'
-
-import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
-
-import blurBg from '../src/assets/bg-blur.png'
-import Stripes from '../src/assets/stripes.svg'
-import NlwLogo from '../src/assets/nlw-spacetime-logo.svg'
-
 import { api } from '../src/lib/api'
 
-const StyledStripes = styled(Stripes)
+import NlwLogo from '../src/assets/nlw-spacetime-logo.svg'
 
 const discovery = {
   authorizationEndpoint: 'https://github.com/login/oauth/authorize',
@@ -31,12 +17,6 @@ const discovery = {
 
 export default function App() {
   const router = useRouter()
-
-  const [hasLoadedFonts] = useFonts({
-    Roboto_400Regular,
-    Roboto_700Bold,
-    BaiJamjuree_700Bold,
-  })
 
   const [, response, githubSignIn] = useAuthRequest(
     {
@@ -71,23 +51,8 @@ export default function App() {
     }
   }, [response, router])
 
-  if (!hasLoadedFonts) {
-    return null
-  }
-
   return (
-    <ImageBackground
-      source={blurBg}
-      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
-      imageStyle={{
-        position: 'absolute',
-        left: '-100%',
-      }}
-    >
-      <StatusBar style="light" />
-
-      <StyledStripes className="absolute left-2" />
-
+    <View className="flex-1 items-center px-8 py-10">
       <View className="flex-1 items-center justify-center gap-6">
         <NlwLogo />
 
@@ -116,6 +81,6 @@ export default function App() {
       <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
         Feito com 💜 no NLW da Rocketseat
       </Text>
-    </ImageBackground>
+    </View>
   )
 }
